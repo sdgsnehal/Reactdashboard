60d8df5fd9d869f8e68008fb48582ce7d8c9325b
sdgsnehal <sdgsnehal@gmail.com>
2023-05-09 11:05:40 +0530

    Bar chart and area Chart

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import { ChartComponent,SeriesCollectionDirective,SeriesDirective,Inject,Legend,Category,Tooltip,ColumnSeries,DataLabel} from '@syncfusion/ej2-react-charts';
+import { barCustomSeries,barPrimaryYAxis,barPrimaryXAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContextProvider';
+import {Header} from '../../components';
+
 
 const Pie = () => {
+  const {currentMode} = useStateContext();
   return (
-    <div>Pie</div>
-  )
-}
+    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
+    <Header category="Area" title="Inflation Rate in Percentage"/>
 
-export default Pie
+    
+    <ChartComponent id="area-chart" height="420px" primaryXAxis={barPrimaryXAxis} primaryYAxis={barPrimaryYAxis} chartArea={{bo
+    :{width:0}}} tooltip={{enable:true}}
+    background={currentMode === 'Dark' ? '#33373E' : '#fff'}>
+      <Inject services={[ColumnSeries,Legend,Tooltip,DataLabel,Category]}/>
+      <SeriesCollectionDirective>
+        {barCustomSeries.map((item,index)=><SeriesDirective key={index} {...item}/>)}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+    </div>
+  )
+} 
+
+export default Pie; 

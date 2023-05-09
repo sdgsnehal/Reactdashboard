60c8732ef44b66c7d2e3e9350c2e81e13d2fddb1
sdgsnehal <sdgsnehal@gmail.com>
2023-05-09 11:16:05 +0530

    Pie chart

--- src/pages/Charts/Pie.jsx
+++ src/pages/Charts/Pie.jsx
@@ -1,27 +1,15 @@
 import React from 'react';
-import { ChartComponent,SeriesCollectionDirective,SeriesDirective,Inject,Legend,Category,Tooltip,ColumnSeries,DataLabel} from '@syncfusion/ej2-react-charts';
-import { barCustomSeries,barPrimaryYAxis,barPrimaryXAxis } from '../../data/dummy';
-import { useStateContext } from '../../contexts/ContextProvider';
-import {Header} from '../../components';
 
+import { pieChartData } from '../../data/dummy';
+import { ChartsHeader, Pie as PieChart } from '../../components';
 
-const Pie = () => {
-  const {currentMode} = useStateContext();
-  return (
-    <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-    <Header category="Area" title="Inflation Rate in Percentage"/>
-
-    
-    <ChartComponent id="area-chart" height="420px" primaryXAxis={barPrimaryXAxis} primaryYAxis={barPrimaryYAxis} chartArea={{bo
-    :{width:0}}} tooltip={{enable:true}}
-    background={currentMode === 'Dark' ? '#33373E' : '#fff'}>
-      <Inject services={[ColumnSeries,Legend,Tooltip,DataLabel,Category]}/>
-      <SeriesCollectionDirective>
-        {barCustomSeries.map((item,index)=><SeriesDirective key={index} {...item}/>)}
-      </SeriesCollectionDirective>
-    </ChartComponent>
+const Pie = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Pie" title="Project Cost Breakdown" />
+    <div className="w-full">
+      <PieChart id="chart-pie" data={pieChartData} legendVisiblity height="full" />
     </div>
-  )
-} 
+  </div>
+);
 
-export default Pie; 
+export default Pie;
